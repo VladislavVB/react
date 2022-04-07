3e13be94cdd66123a3eb9c279238a5b211245f72
Vladislav <burdukovskij.vlad@mail.ru>
2022-04-07 19:37:34 +0300

    ffffff

--- first/src/App.js
+++ first/src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+
 
 function App() {
+  const [likes, setLikes] = useState(5);
+  const [value, setValue] = useState('fdsfdsf');
+
+  const pluslike = () => {
+    setLikes(likes + 1)
+  }
+  const minusLike = () => {
+    setLikes(likes - 1)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{likes}</h1>
+      <button onClick={pluslike}>Plus</button>
+      <button onClick={minusLike}>Minus</button>
+      <input 
+        placeholder='input' 
+        onChange={event => setValue(event.target.value)} 
+        value={value} 
+      />
+      <h1>{value}</h1>
     </div>
   );
 }
